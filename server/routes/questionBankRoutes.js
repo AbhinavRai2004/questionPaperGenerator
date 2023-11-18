195f1dfd6da3881dcf0140135587bfd2bc261583
Abhinav <raiabhinav.in@gmail.com>
2023-11-18 22:14:20 +0530

    Routes File

---
+++ server/routes/questionBankRoutes.js
@@ -0,0 +1,10 @@
+const express = require('express');
+
+const { questionBankController} = require('../controller/questionBankController');
+
+const router = express.Router();
+
+router.post('/',questionBankController);
+
+module.exports = router;
+
