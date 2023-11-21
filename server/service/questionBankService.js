d2825b2e061902640fbb756a3265e4cb07698ab9
Abhinav <raiabhinav.in@gmail.com>
2023-11-21 23:05:12 +0530

    Edge case handled

--- server/service/questionBankService.js
+++ server/service/questionBankService.js
@@ -23,24 +23,24 @@ const quesBankGenerator = (req) => {
     
     if(userInput['totalMark'] < 0 || userInput['easy'] < 0 || userInput['medium'] < 0 || userInput['hard'] < 0)
     {
-        // console.log("Negative parameters are not allowed");
-        return {message : "Negative parameters are not allowed"};
+        // console.log("Negative values are not allowed for total marks or difficulty levels.");
+        return {message: "Error: Negative values are not allowed for total marks or difficulty levels."};
     }
     
     if((((userInput['totalMark']*userInput['easy'])/100) % 5 != 0) && 
        (((userInput['totalMark']*userInput['medium'])/100) % 10 != 0) &&
        (((userInput['totalMark']*userInput['hard'])/100) % 15 != 0))
     {
-        // console.log("total mark can not distributed in given percentage");
-        return {message : "total mark can not distributed in given percentage"};
+        // console.log(" The total marks cannot be distributed according to the specified percentages.");
+        return { message: "Error: The total marks cannot be distributed according to the specified percentages."};
     }
 
     if((userInput['totalMark'] <= ((userInput['easy']*userInput['totalMark'])/100)) ||
        (userInput['totalMark'] <= ((userInput['medium']*userInput['totalMark'])/100)) ||
        (userInput['totalMark'] <= ((userInput['hard']*userInput['totalMark'])/100))  )
     {
-        // console.log("Difficulty percentage is not more than total percentage");
-        return {message : "Difficulty percentage is not more than total percentage" };
+        // console.log("The sum of difficulty percentages exceeds the total percentage of marks.");
+        return { message: "Error: The sum of difficulty percentages exceeds the total percentage of marks." };
     }
 
     // Ensure the result is an integer
