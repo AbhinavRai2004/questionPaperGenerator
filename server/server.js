544e4b256aa3ac2a6a6c9ab22aefacddff27f9a4
Abhinav <raiabhinav.in@gmail.com>
2023-11-23 21:11:24 +0530

    Server File

--- server/server.js
+++ server/server.js
@@ -1,10 +1,12 @@
 const express = require('express');
 const Router = require('./routes/questionBankRoutes');
+const testRouter = require('./routes/testRoutes');
+
 
 const app = express();
 app.use(express.json());
 app.use('/',Router);
-
+ 
 const PORT = 8080;
 
 app.listen(PORT,() => {
