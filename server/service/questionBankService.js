8f6eefde9be8ef4900461b79de5c6b412c9fef3f
Abhinav <raiabhinav.in@gmail.com>
2023-11-24 23:53:31 +0530

    Improvement in response

--- server/service/questionBankService.js
+++ server/service/questionBankService.js
@@ -1,6 +1,7 @@
 const quesSample = require('../questionStore');
 
-// Function to shuffle an array using Fisher-Yates algorithm
+// Function to shuffle an array using Fisher-Yates algorithm.
+
 const shuffleArray = (array) => {
     for (let i = array.length - 1; i > 0; i--) {
         const j = Math.floor(Math.random() * (i + 1));
@@ -12,29 +13,33 @@ const quesBankGenerator = (req) => {
     const userInput = req.body;
 
     // handling edge cases.
+
     if (!userInput || !userInput['totalMarks'] || !userInput['easy'] || !userInput['medium'] || !userInput['hard'])
     {
         // console.log('Missing required parameters');
-        return {status : 400 , returnObj : "Error : Missing Required Parameters"};
+        return {status : 400 , message : "Error : Missing Required Parameters"};
     }
     if(userInput['totalMarks'] < 0 || userInput['easy'] < 0 || userInput['medium'] < 0 || userInput['hard'] < 0)
     {
         // console.log("Negative values are not allowed for total marks or difficulty levels.");
-        return {status : 400 , returnObj : "Error: Negative values are not allowed for total marks or difficulty levels."};
+        return {status : 400 , message : "Error: Negative values are not allowed for total marks or difficulty levels."};
     }
     if((((userInput['totalMarks']*userInput['easy'])/100) % 5 != 0) || 
-    (((userInput['totalMarks']*userInput['medium'])/100) % 10 != 0) ||
-    (((userInput['totalMarks']*userInput['hard'])/100) % 15 != 0))
+      (((userInput['totalMarks']*userInput['medium'])/100) % 10 != 0) ||
+      (((userInput['totalMarks']*userInput['hard'])/100) % 15 != 0))
     {
-     // console.log(" The total marks cannot be distributed according to the specified percentages.");
-     return { status: 400, returnObj: "Error: The total marks cannot be distributed according to the specified percentages."};
+    //  console.log(" The total marks cannot be distributed according to the specified percentages.");
+     return { status: 400, message : { "Error" : [ "The total marks cannot be distributed according to the specified percentages.",
+      "Easy Question Mark Distribution must be in multiple of 5", 
+      "Medium Question Mark Distribution must be in multiple of 10",
+      "Hard Question Mark Distribution must be in multiple of 15"]}};
     }
     if((userInput['easy'] + userInput['medium'] + userInput['hard'])  != 100 )
     {
         // console.log("The sum of difficulty percentages must equal to 100.");
-        return {status : 400, returnObj : "Error: The sum of difficulty percentages must equal to 100." };
+        return {status : 400, message : "Error: The sum of difficulty percentages must equal to 100." };
     }
-    
+
     const easyQuestionCn = Math.ceil(((userInput['totalMarks'] * userInput['easy']) / 100)/5);
     const mediumQuestionCn = Math.ceil(((userInput['totalMarks'] * userInput['medium']) / 100)/10);
     const hardQuestionCn = Math.ceil(((userInput['totalMarks'] * userInput['hard']) / 100)/15);
